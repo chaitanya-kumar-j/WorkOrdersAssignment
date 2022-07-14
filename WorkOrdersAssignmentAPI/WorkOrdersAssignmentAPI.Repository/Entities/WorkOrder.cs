5b23db71f41105f2edc9cd9ed0be7d3fa6f7547f
chaitanya4294 <chaitanya.4294@gmail.com>
2022-07-14 16:50:16 +0530

    [Chaitanya] Add: Added folder structure and entities

---
+++ WorkOrdersAssignmentAPI/WorkOrdersAssignmentAPI.Repository/Entities/WorkOrder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkOrdersAssignmentAPI.Repository.Entities
+{
+    public class WorkOrder
+    {
+        [Key]
+        public string WorkOrderId { get; set; } = string.Empty;
+       
+        [Required]
+        public string Place { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
